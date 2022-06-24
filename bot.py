274c1fc63d323d9bb0a4c6c7ac9c4c2e45b64896
gaevalja <starararara@naver.com>
2022-06-25 02:32:20 +0900

    firstbot06

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import datetime
+
+
+app = commands.Bot(command_prefix='/')
+
+
+
+@app.event
+async def on_ready():
+    print('Done')
+    await app.change_presence(status=discord.Status.online, activity=None)
+
+
+
+@app.command()
+async def startGettingMoney(ctx):
+    await ctx.send("#주의#/startGettingMoney 명령어를 한번 더 사용하시면 total value가 초기화됩니다. 물론 김성주가 컴퓨터를 끄면 초기화되겠지용 ㅋ")
+    total = 0
+    n = 0
+    dt = datetime.datetime.now()
+    now_min = dt.minute
+    while 1:
+        if now_min != dt.minute:
+            
+            now_min = dt.minute
+            total+=2.28310502283105
+            n+=1
+            await ctx.send("%d분동안 %lf원이 적립되었습니당 ㅋㅎㅋㅎ"%(n, total))    
+        dt = datetime.datetime.now()
+        
+
+app.run('OTg1MTEzNzM5MDcyMjcwMzM3.GpGIfe.6HSOe2QM84FlMLwBbUOdIBsTxnWj9hQDymSdYs')
+
+
+
+
